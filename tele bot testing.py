9260c39804d57a5cb4dea5cff014a0881e20f5c2
matthewyip1511 <116422270+matthewyip1511@users.noreply.github.com>
2022-10-30 16:17:29 +0800

    Add files via upload

--- tele bot testing.py
+++ tele bot testing.py
@@ -4,73 +4,82 @@ from telegram.ext.callbackcontext import CallbackContext
 from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
-  
-updater = Updater("5686629010:AAEyvTD61I9aNzOIKHxO2yvXm0rR_Mfr9Ds",
+import datetime
+import json
+
+#GPT-3 AI testing
+import os
+import openai
+
+
+updater = Updater("5780822067:AAEM7xgo635DC4U-_VbiR7kGdEwFGiqglt4",
                   use_context=True)
-  
   
 def start(update: Update, context: CallbackContext):
     update.message.reply_text(
-        "hello good afternoon welcome to the highway. nice of you to join us!")
+       "Hello, welcome to the COVID-19 response bot. Please enter your questions / concerns to allow us to direct you to the right channels.")
+    print('user started the bot!')
+    
 
 def help(update: Update, context: CallbackContext):
-    update.message.reply_text("nah no help available")
+    #tells the user what the bot is about and what they can do with it
+    update.message.reply_text("For common questions regarding COVID-19 in Singapore, do read the latest information on the MOH website")
 
-def raisehand(update: Update, context: CallbackContext):
-    update.message.reply_text("noted i see u")
-    
-##def help(update: Update, context: CallbackContext):
-##    update.message.reply_text("""Available Commands :-
-##    /youtube - To get the youtube URL
-##    /linkedin - To get the LinkedIn profile URL
-##    /gmail - To get gmail URL
-##    /geeks - To get the GeeksforGeeks URL""")
-  
-  
-def gmail_url(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        "Your gmail link here (I am not\
-        giving mine one for security reasons)")
-  
-  
-def youtube_url(update: Update, context: CallbackContext):
-    update.message.reply_text("Youtube Link =>\
-    https://www.youtube.com/")
-  
-  
-def linkedIn_url(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        "LinkedIn URL => \
-        https://www.linkedin.com/in/dwaipayan-bandyopadhyay-007a/")
-  
-  
-def geeks_url(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        "GeeksforGeeks URL => https://www.geeksforgeeks.org/")
-  
   
 def unknown(update: Update, context: CallbackContext):
     update.message.reply_text(
         "Sorry '%s' is not a valid command" % update.message.text)
+
+def texts(update: Update, context: CallbackContext):
+    #receive text input from user and save it as text
+    text = update.message.text.lower()
+
+    openai.api_key = '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' # my openai key
+
+    response = openai.Completion.create(
+      model="text-davinci-002",
+      prompt="Classify the urgency of a message as 0,1,2,3\n\nDefinition: Spam, nonsense messages. Unrelated to health issues.\nMessage: “fjakljfklasjfsadasl”\nUrgency: 0 \n\n##\n\nDefinition: User needs help but it is not time-critical at all, users can read the answers themselves on the Ministry of Health website\nMessages:  “what is covid”, “what is the number of cases today”\nUrgency: 1\n\n##\n\nDefinition: User needs help but help can be provided through an automated chatbot / text\nMessages: “I have covid can I go out\", “I am coughing what medicine can i get”, “I have covid but I share room with my brother where do I go”\nUrgency: 2\n\n##\nDefinition: User has serious issues that require the immediate attention of a call centre\nMessages: \"I cannot breathe, what do i do”, “My throat hurts… where can I go”\nUrgency: 3\n\nUser message: " + text + "\nUrgency:\n\n\n\n",
+      temperature=0.7,
+      max_tokens=256,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0
+    )
+
+    response_dict = json.loads(str(response))
+    print('response dict is', response_dict)
+    urgency = int(response_dict['choices'][0]['text'])
+    print(urgency)
+
+    if urgency == 0:
+        update.message.reply_text("Please enter a valid message")
+
+    elif urgency == 1:
+        update.message.reply_text("You will be able to seek the answer to this query on the MOH website at \nmoh.gov.sg/faqs")
+
+    elif urgency == 2:
+        update.message.reply_text("Please seek further help at the chatbot below \nt.me/covidhelp")  #dummy bot work in progress
+
+    elif urgency == 3:
+        update.message.reply_text("Please hold on and do not leave your house. An assistant would be calling you shortly")
+        
+
+    print('reply given to user!')
   
-  
-def unknown_text(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        "Sorry I can't recognize you , you said '%s'" % update.message.text)
-  
-  
+
+#to handle incoming commands
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('raisehand', raisehand))
-updater.dispatcher.add_handler(CommandHandler('youtube', youtube_url))
 updater.dispatcher.add_handler(CommandHandler('help', help))
-updater.dispatcher.add_handler(CommandHandler('linkedin', linkedIn_url))
-updater.dispatcher.add_handler(CommandHandler('gmail', gmail_url))
-updater.dispatcher.add_handler(CommandHandler('geeks', geeks_url))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
-updater.dispatcher.add_handler(MessageHandler(
-    Filters.command, unknown))  # Filters out unknown commands
+updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown))  # Filters out unknown commands
+
+#line to deal with texts 
+updater.dispatcher.add_handler(MessageHandler(Filters.text, texts))
   
 # Filters out unknown messages.
 updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
-  
+updater.dispatcher.add_handler(MessageHandler(Filters.text, echo)) 
+
+#start running the bot proper
 updater.start_polling()
+
+
